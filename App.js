99d7a880771e01a2b38a3dac224b27ca994a2b04
manjotsinghsuri <msmanjotsinghms13@gmail.com>
2020-07-30 18:50:42 +0530

    Added backend

--- App.js
+++ App.js
@@ -2,8 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import NotesScreenComponent from './src/NotesScreenComponent';
+import firebase from 'firebase';
 
 export default function App() {
+
+
+if(firebase.apps.length===0) {
+  var firebaseConfig = {
+    apiKey: "AIzaSyC4CSg8xmZsZ-3vzaunu6Y5H9x19H6yS_k",
+    authDomain: "rn-todo-list-81080.firebaseapp.com",
+    databaseURL: "https://rn-todo-list-81080.firebaseio.com",
+    projectId: "rn-todo-list-81080",
+    storageBucket: "rn-todo-list-81080.appspot.com",
+    messagingSenderId: "376820207298",
+    appId: "1:376820207298:web:40005391e95505a451755b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
+
   return (
     <View style={styles.container}>
       <NotesScreenComponent/>
